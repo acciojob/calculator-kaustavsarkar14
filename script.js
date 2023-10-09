bdd7864e0d77ede7c18cadb850dfef4d3b9cb4f3
Kaustav Sarkar <93545845+kaustavsarkar14@users.noreply.github.com>
2023-10-09 11:23:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your code here
+const input = document.getElementById('input')
+
+function clearInput(){
+    input.value = ""
+}
+
+function addToInput(num){
+    input.value+=num
+}
+
+function backspace(){
+    input.value = input.value.slice(0, -1)
+}
+
+function ans(){
+    input.value = eval(input.value)
+}
